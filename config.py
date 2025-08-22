d121c486cd10b6d991d8ff6affe5727e48f9e32f
Oleksandr <oleksandr@example.com>
2025-08-22 09:54:27 +0300

    moved sqlite to Fly Volume

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 # --- Налаштування бази даних ---
 # ПОВЕРТАЄМОСЬ ДО ПРОСТОЇ НАЗВИ ФАЙЛУ
-DB_NAME = os.getenv("DB_NAME", "signals.db")
+DB_NAME = os.getenv("DB_NAME", "/data/signals.db")
 
 # --- Telegram ---
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
