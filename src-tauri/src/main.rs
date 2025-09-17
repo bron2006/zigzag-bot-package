ea17efce9f579994830ccc196f13151e50111bf7
Oleksandr <oleksandr@example.com>
2025-09-18 02:42:01 +0300

    Reset to default tauri main.rs and simplify dependencies

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,19 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::window::WindowUrl; // Правильний шлях до WindowUrl
-
 fn main() {
-    tauri::Builder::default()
-        .setup(|app| {
-            tauri::WindowBuilder::new(app, "main")
-                .title("ZigZag Terminal")
-                .with_url(WindowUrl::External( // Правильний метод .with_url()
-                    "http://localhost:8080".parse().unwrap(),
-                ))
-                .build()?;
-            Ok(())
-        })
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+  tauri::Builder::default()
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
